b9bc64b0cd6afa3a5bb2e4688032703dcb94d1cc
Basil Labib <basillabib01@gmail.com>
2022-11-02 09:35:57 +0530

    Implement: Formik form, add validation and state management to form fields

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   Image,
@@ -7,24 +7,42 @@ import {
   Text,
   FormControl,
   FormLabel,
+  FormErrorMessage,
   Input,
   Spacer,
   Button,
   Square,
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
 
 function Login() {
+  const validateEmail = values => {
+    const errors = {};
+
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = 'Invalid email address';
+    }
+
+    return errors;
+  };
+
   return (
     <Box>
       <Flex>
-        <Box p='4' pl="8">
+        <Box p="4" pl="8">
           <Flex>
             <Center w="75px">
               <Image boxSize="40px" objectFit="cover" src="./logo.svg" />
             </Center>
             <Square size="90px">
-              <Text fontSize={'lg'} fontWeight={'bold'}>Ant Farm</Text>
+              <Text fontSize={'lg'} fontWeight={'bold'}>
+                Ant Farm
+              </Text>
             </Square>
           </Flex>
           <Box p="2">
@@ -36,7 +54,7 @@ function Login() {
 
           <Box p="4">
             <Center>
-              <Button  border='1px' borderColor={'gray.400'} bg='white'>
+              <Button border="1px" borderColor={'gray.400'} bg="white">
                 <Center>
                   <Image boxSize={4} src="./googlelogo.png"></Image>
                   <Text>Sign up with Google</Text>
@@ -48,53 +66,88 @@ function Login() {
           <hr />
 
           <Box p="4">
-            <FormControl >
-              <FormLabel>
-                <Text color={'gray.500'}>
-                  Email*
-                </Text>
-              </FormLabel>
-              <Input
-                placeholder="johndoe@example.com"
-                htmlSize={4}
-                type="email"
-              />
-            </FormControl>
-
             <Box p="2"></Box>
-            <FormControl size={'sm'}>
-              <FormLabel>
-                <Flex>
-                  <Text color={'gray.500'}>
-                    Password*
-                  </Text>
-                  <Spacer />
-                  <Text color="brand.400">
-                    <Link to="/forgotpassword">Forgot Password?</Link>
-                  </Text>
-                </Flex>
-              </FormLabel>
-              <Input
-                placeholder="Minimum 6 characters"
-                htmlSize={2}
-                type="password"
-              />
-            </FormControl>
+            <Formik
+              initialValues={{ email: '', password: '' }}
+              validate={validateEmail}
+              onSubmit={(values, setSubmitting) => {
 
-            <Box p="2"></Box>
-            <Flex>
-              <Text fontSize={'sm'} color={'gray.400'}>
-                Don't have an account? &nbsp;{' '}
-              </Text>
-              <Text fontSize={'sm'} color={'brand.400'}>
-                <Link to="/signup">Create a new account here!</Link>
-              </Text>
-            </Flex>
+                // make POST request here
+                // values.email and values.password
 
-            <Box pt="8"></Box>
-            <Button colorScheme="brand" size="md">
-              Login to your Dashboard
-            </Button>
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+                }, 400);
+              }}
+            >
+              {({ values, errors, handleSubmit, isSubmitting }) => (
+                <Form>
+                  <Field name="email">
+                    {({ field, form }) => (
+                      <FormControl
+                        isInvalid={form.errors.email && form.touched.email}
+                      >
+                        <FormLabel>
+                          <Text color={'gray.500'}>Email*</Text>
+                        </FormLabel>
+                        <Input {...field} value={values.email} />
+                        <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+
+                  <Field name="password">
+                    {({ field, form }) => (
+                      <FormControl
+                        size={'sm'}
+                        isInvalid={
+                          form.errors.password && form.touched.password
+                        }
+                      >
+                        <FormLabel>
+                          <Flex>
+                            <Text color={'gray.500'}>Password*</Text>
+                            <Spacer />
+                            <Text color="brand.400">
+                              <Link to="/forgotpassword">Forgot Password?</Link>
+                            </Text>
+                          </Flex>
+                        </FormLabel>
+                        <Input
+                          {...field}
+                          type="password"
+                          value={values.password}
+                        />
+                        <FormErrorMessage>
+                          {form.errors.password}
+                        </FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+
+                  <Box p="2"></Box>
+                  <Flex>
+                    <Text fontSize={'sm'} color={'gray.400'}>
+                      Don't have an account? &nbsp;{' '}
+                    </Text>
+                    <Text fontSize={'sm'} color={'brand.400'}>
+                      <Link to="/signup">Create a new account here!</Link>
+                    </Text>
+                  </Flex>
+
+                  <Box pt="8"></Box>
+                  <Button
+                    colorScheme="brand"
+                    size="md"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Login to your Dashboard
+                  </Button>
+                </Form>
+              )}
+            </Formik>
           </Box>
         </Box>
         <Spacer />
